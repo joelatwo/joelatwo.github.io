59d217b04079021ec42291fc91f4021e01480f23
atwo6777 <haystackjma@gmail.com>
2019-01-03 18:33:07 -0800

    Updates

---
+++ precache-manifest.8afdbc0051e0b0b46f1753e148e3e0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1de3c7dc57d75ce5b95e",
+    "url": "/static/js/main.1de3c7dc.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1de3c7dc57d75ce5b95e",
+    "url": "/static/css/main.c821c5c2.chunk.css"
+  },
+  {
+    "revision": "4bd7dae98493fa28adce686064547a16",
+    "url": "/index.html"
+  }
+];
