ef61e2b4566dc28c18533bc2a8717102d75da351
joelatwo <joelatwo@gmail.com>
2026-06-20 20:41:47 -0600

    More of all the things

--- app/pages/projects/[slug].vue
+++ app/pages/projects/[slug].vue
@@ -6,7 +6,7 @@
 const route = useRoute();
 const slug = route.params.slug as string;
 const { data } = await useAsyncData(() =>
-  queryContent("content").path(`/projects/${slug}`).first()
+  queryContent("content").path(`/projects/${slug}`).first(),
 );
 console.log(slug, data, data.value);
 </script>
